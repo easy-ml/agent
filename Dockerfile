09b5e229aaca8cfe458f1a2774e307a47d4f8959
Rustem Alkapov <ralkapov@gmail.com>
2019-03-18 00:17:59 +0500

    fix: update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,9 @@ COPY . /code
 WORKDIR /code
 
 ENV PYTHONUNBUFFERED 1
+ENV LC_ALL C.UTF-8
+ENV LANG C.UTF-8
+
 RUN pip3 install -r requirements.txt
 RUN pip3 install .
+CMD run-agent
--- agent/agent.py
+++ agent/agent.py
@@ -2,6 +2,7 @@ import logging
 
 import click
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(name)s: %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
